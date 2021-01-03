b5deefb2bb6924d1d5675150fac7571f55226e43
bicarlsen <carlsen.bri@gmail.com>
2021-01-03 12:03:59 +0100

    Updated package version.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ project_urls = {
 
 setuptools.setup(
 	name='thot-data',
-	version = '0.4.4',
+	version = '0.4.5',
 	author='Brian Carlsen',
 	author_email = 'carlsen.bri@gmail.com',
 	description = 'Thot data analysis and management.',
