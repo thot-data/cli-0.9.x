8d23757dbbf4190507265a82c71634f0f891f000
bicarlsen <carlsen.bri@gmail.com>
2021-03-01 21:31:24 +0100

    Updated version.

--- setup.py
+++ setup.py
@@ -1,42 +1,42 @@
 import setuptools
 
 with open( 'README.rst', 'r' ) as f:
-	long_desc = f.read()
+    long_desc = f.read()
 
 classifiers = [
-	"Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3",
     "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     "Operating System :: OS Independent",
 ]
 
 project_urls = {
-	'Documentation': 	'https://thot-data-docs.readthedocs.io/',
-	'Source Code': 		'https://github.com/bicarlsen/thot-data',
-	'Bug Tracker':		'https://github.com/bicarlsen/thot-data/issues'
+    'Documentation':    'https://thot-data-docs.readthedocs.io/',
+    'Source Code':      'https://github.com/bicarlsen/thot-data',
+    'Bug Tracker':      'https://github.com/bicarlsen/thot-data/issues'
 }
 
 
 setuptools.setup(
-	name='thot-data',
-	version = '0.4.7',
-	author='Brian Carlsen',
-	author_email = 'carlsen.bri@gmail.com',
-	description = 'Thot data analysis and management.',
-	long_description = long_desc,
-	long_description_content_type = 'text/x-rst',
-	url = 'http://www.thot-data.com',
-	packages = setuptools.find_packages(),
-	project_urls = project_urls,
-	classifiers = classifiers,
-	
-	install_requires = [
-		'thot-core>=0.4.5'
-	],
+    name='thot-data',
+    version = '0.4.8',
+    author='Brian Carlsen',
+    author_email = 'carlsen.bri@gmail.com',
+    description = 'Thot data analysis and management.',
+    long_description = long_desc,
+    long_description_content_type = 'text/x-rst',
+    url = 'http://www.thot-data.com',
+    packages = setuptools.find_packages(),
+    project_urls = project_urls,
+    classifiers = classifiers,
+    
+    install_requires = [
+        'thot-core>=0.4.8'
+    ],
 
-	package_data = {
-	},
+    package_data = {
+    },
 
-	entry_points = {
-		'console_scripts': []
-	}
+    entry_points = {
+        'console_scripts': []
+    }
 )
